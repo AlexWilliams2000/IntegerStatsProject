5bb845b6a38a07ba053186706d28a0a4b7b63fd4
AlexWilliams <alex.williams2000@hotmail.co.uk>
2017-09-25 20:28:23 +0100

    Stop execution if no input file is specified

--- src/main/java/personal/alex/IntegerStatsControl.java
+++ src/main/java/personal/alex/IntegerStatsControl.java
@@ -8,6 +8,15 @@ package personal.alex;
 public class IntegerStatsControl
 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length == 0) {
+            String errorNoFileNameArgument = "Error: No file was specified\n\n"
+                    + "Usage: java {file location and name}\n"
+                    + "\tExample: java C:\\myFolder\\myCSV.csv\n";
+            System.out.println(errorNoFileNameArgument);
+            System.exit(1);
+        }
+        else {
+            System.out.println(args[0]);
+        }
     }
 }
