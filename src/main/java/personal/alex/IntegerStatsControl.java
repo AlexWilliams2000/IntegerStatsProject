0436bf343f729422ccbe15377d59d62497481cff
AlexWilliams <alex.williams2000@hotmail.co.uk>
2017-09-25 20:17:38 +0100

    Initial commit
    Basic project structure

---
+++ src/main/java/personal/alex/IntegerStatsControl.java
@@ -0,0 +1,13 @@
+package personal.alex;
+
+/**
+ * Integer statistics main class
+ *  Alex W. 09/2017
+ */
+
+public class IntegerStatsControl
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/personal/alex/IntegerStatsControlTest.java
@@ -0,0 +1,5 @@
+package personal.alex;
+
+public class IntegerStatsControlTest {
+
+}
