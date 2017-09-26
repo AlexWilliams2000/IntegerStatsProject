90658e9c8ac6711365cd63c200c7cb42f286b1d6
AlexWilliams <alex.williams2000@hotmail.co.uk>
2017-09-26 22:44:29 +0100

    Added test class stub for simple stats engine

--- src/main/java/personal/alex/SimpleIntegerStatsEngine.java
+++ src/main/java/personal/alex/SimpleIntegerStatsEngine.java
@@ -1,6 +1,16 @@
 package personal.alex;
 
+import java.util.List;
+
 public class SimpleIntegerStatsEngine implements IntegerStatsEngine {
+    private List<int[]> inputIntegers;
+    private List<String> calculatedIntegerStats;
+
+    //Todo: Total number of integers
+    //Todo: Mean value of integers to 3dp
+    //Todo: Highest number of integers in a line
+    //Todo: Mode
+
     public void setIntegerData() {};
     public void getIntegerData() {};
     public void calculateIntegerStats() {};
