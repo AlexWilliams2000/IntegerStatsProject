829f980dcd843261638cb7eb8175ebbbce1c8049
AlexWilliams <alex.williams2000@hotmail.co.uk>
2017-09-27 02:19:34 +0100

    Added terminal display functionality to SimpleDisplay class

--- src/main/java/personal/alex/SimpleDisplay.java
+++ src/main/java/personal/alex/SimpleDisplay.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 public class SimpleDisplay implements Display {
 
+    static final String header = "Calculated Integer Stats:\n\n";
+
     public SimpleDisplay() {
 
     }
 
     public void display(List<String> infoToDisplay) {
+        System.out.println(header);
         for(String line : infoToDisplay) {
-
+            System.out.println(line);
         }
     }
 }
